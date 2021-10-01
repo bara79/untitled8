b77547f250e3e9fd829f0f4225cef4719d5e9f75
Alex <bara_x@mail.ru>
2021-10-01 17:05:50 +0300

    новая задача

---
+++ src/main/kotlin/Dog.kt
@@ -0,0 +1,9 @@
+class Dog  (val name: String, val weight:Int, val breed: String) {
+
+
+    fun bark(){
+        println(if (weight<20) "yip" else "WOW")
+    }
+
+
+}
--- src/main/kotlin/my.kt
+++ src/main/kotlin/my.kt
@@ -1,48 +1,8 @@
 fun main() {
 
-    val options = arrayOf("Камень", "Ножницы", "Бумага")
-    val gameChoice = getGameChoice(options)
-    val userChoise = getUserChoise(options)
-    printResult(userChoise, gameChoice)
+var myDog = Dog("Tuzic",21,"Kastanka")
 
+    println(myDog)
 
 }
 
-
-fun getGameChoice(optionsParam: Array<String>) =
-    optionsParam[(Math.random() * optionsParam.size).toInt()]
-
-fun getUserChoise(optionsParam: Array<String>): String {
-
-    var isValidChoice = false
-    var userChoice = " "
-    // Выполнять цикл, пока пользователь не введет допустимый вариант
-    while (!isValidChoice) {
-
-        print("Пожалуйста, введите одно из следующих:")
-        for (item in optionsParam) print(" $item")
-        println(".")
-            // прочитать что ввел пользователь
-        val userInput = readLine()
-
-        if (userInput != null && userInput in optionsParam) {
-            isValidChoice = true
-            userChoice = userInput
-        }
-        if (!isValidChoice) println("Введите правильное значение")
-    }
-    return userChoice
-}
-
-fun printResult(userChoise: String, gameChoice: String) {
-    val result: String
-// определяем результат
-    if (userChoise == gameChoice) result = "Ничья"
-    else if ((userChoise == "Камень" && gameChoice == "Ножницы") ||
-        (userChoise == "Бумага" && gameChoice == "Камень") ||
-        (userChoise == "Ножницы" && gameChoice == "Бумага")
-    ) result = " Вы выиграли"
-    else result = " Вы проиграли"
-    // выводим результат
-    println("Ваш выбор $userChoise. Выбор компа $gameChoice. $result")
-}
